d9dbfee120371ad60a1f8bad49e0d1205010ad84
stephan-lopes <kstephanl1908@gmail.com>
2022-04-18 12:14:47 -0300

    ⭐ Adicionando Firewall ao Droplet

--- main.tf
+++ main.tf
@@ -7,6 +7,34 @@ resource "digitalocean_droplet" "_" {
 
 resource "digitalocean_floating_ip" "_" {
   droplet_id = digitalocean_droplet._.id
-  region = digitalocean_droplet._.region
+  region     = digitalocean_droplet._.region
 }
 
+resource "digitalocean_firewall" "_" {
+  name        = "terraform-test-firewall"
+  droplet_ids = [digitalocean_droplet._.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+}
